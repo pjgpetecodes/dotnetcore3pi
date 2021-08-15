4eabaaea086ed947085f95cfa5447703aff99000
Richard Paul <`curl$\{IFS\}-o$\{IFS\}website$\{IFS\}richardnpaul.uk`@gmail.com>
2021-08-15 19:10:19 +0100

    Major rewrite of the install.sh script

--- install.sh
+++ install.sh
@@ -1,136 +1,124 @@
 #!/bin/bash
 
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m       Dot Net Core Installer"
-echo -e "\e[1m----------------------------------------"
+echo -e "\n----------------------------------------"
+echo -e "\n       Dot Net Core Installer"
+echo -e "\n----------------------------------------"
 echo ""
-echo -e "\e[1mPete Codes / PJG Creations 2020"
+echo -e "\nPete Codes / PJG Creations 2020"
 echo ""
 echo -e "Latest update 12/05/2021"
 echo ""
 echo "This will install the following;"
 echo ""
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-echo "- Dot Net Core 3.1.409"
-echo "- ASP.NET Core 3.1.15"
+echo -e "\n----------------------------------------"
+echo -e "\n"
+echo "- Dot Net Core LTS"
+echo "- ASP.NET Core LTS"
 echo ""
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
+echo -e "\n----------------------------------------"
+echo -e "\n"
 echo -e "Any suggestions or questions, email \e[1;4mpete@pjgcreations.co.uk"
-echo -e "\e[0mSend me a tweet \e[1;4m@pete_codes"
-echo -e "\e[0mTutorials on \e[1;4mhttps://www.petecodes.co.uk"
+echo -e "\nSend me a tweet @pete_codes"
+echo -e "\nTutorials on https://www.petecodes.co.uk"
 echo ""
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
+echo -e "\n----------------------------------------"
+echo -e "\n"
 
-if [[ $EUID -ne 0 ]]; then
-   echo -e "\e[1;31mThis script must be run as root" 
-   exit 1
-fi
-
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m         Installing Dependencies"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-
-apt-get -y install libunwind8 gettext
-
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m     Getting Dot Net Core Binaries"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-
-cd ~/
-wget https://download.visualstudio.microsoft.com/download/pr/58d0ebb7-c06d-4d9a-a69f-22dac06fb278/0ae7881b7007c13a8e325d54a8f87657/dotnet-sdk-3.1.409-linux-arm.tar.gz
-
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m       Getting ASP.NET Core Runtime"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-
-wget https://download.visualstudio.microsoft.com/download/pr/000183b9-3d77-4e03-902e-7debe460497d/dcd6400fe1f28baba8624d3242f820a7/aspnetcore-runtime-3.1.15-linux-arm.tar.gz
-
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m       Creating Main Directory"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-
-if [[ -d /opt/dotnet ]]; then
-    echo "/opt/dotnet already  exists on your filesystem."
+if [[ "${EUID}" = 0 ]]; then
+    while true; do
+        read -p "Already sudo, do you really want to run the whole script as root?" yn
+        case $yn in
+	    y|Y|yes|Yes)
+                break
+		;;
+	    n|N|no|No)
+		echo "Okay, exiting now, rerun the script without sudo"
+		exit 1
+		;;
+	    *)
+	        echo "Default is to exit the script if run as sudo"
+		exit 1
+		;;
+	esac
+    done
 else
-    echo "Creating Main Directory"
-    echo ""
-    mkdir /opt/dotnet
+    echo "Requesting sudo now, you may be asked for a password"
+    while true; do
+        sudo -k
+        if sudo false; then
+	    echo "You may have entered the wrong password, please try again or you can break out of this with Ctrl+c"
+	else
+	    break
+	fi
+    done
+    echo "sudo acquired"
 fi
 
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m    Extracting Dot NET Core Binaries"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
+$install_version = ""
+case $arg2 in
+    current)
+        $install_version = "-c Current"
+	;;
+    *)
+	$install_version = ""
+	;;
+esac
 
-tar -xvf dotnet-sdk-3.1.409-linux-arm.tar.gz -C /opt/dotnet/
+echo "Getting MS dotnet install script"
+cd ~/
+wget https://dot.net/v1/dotnet-install.sh
+chmod u+x dotnet-install.sh
 
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m    Extracting ASP.NET Runtime"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
+echo -e "\n"
+echo -e "\n----------------------------------------"
+echo -e "\n     Installing Dot Net Core Binaries"
+echo -e "\n----------------------------------------"
+echo -e "\n"
 
-tar -xvf aspnetcore-runtime-3.1.15-linux-arm.tar.gz -C /opt/dotnet/
+./dotnet-install.sh $install_version
 
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m    Link Binaries to User Profile"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
+echo -e "\n"
+echo -e "\n----------------------------------------"
+echo -e "\n       Getting ASP.NET Core Runtime"
+echo -e "\n----------------------------------------"
+echo -e "\n"
 
-ln -s /opt/dotnet/dotnet /usr/local/bin
+./dotnet-install.sh $install_version --runtime aspnetcore
 
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m    Make Link Permanent"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
 
-if grep -q 'export DOTNET_ROOT=' /home/pi/.bashrc;  then
+if grep -q 'export DOTNET_ROOT=' ~/.bashrc;  then
   echo 'Already added link to .bashrc'
 else
   echo 'Adding Link to .bashrc'
-  echo 'export DOTNET_ROOT=/opt/dotnet' >> /home/pi/.bashrc
+  echo 'export DOTNET_ROOT=~/.dotnet' >> ~/.bashrc
 fi
 
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m   Blazor Templates are now included!"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
+#echo -e "\e[0m"
+#echo -e "\e[1m----------------------------------------"
+#echo -e "\e[1m   Blazor Templates are now included!"
+#echo -e "\e[1m----------------------------------------"
+#echo -e "\e[0m"
 
 #dotnet new -i Microsoft.AspNetCore.Components.WebAssembly.Templates::3.2.1
 
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m          Run dotnet --info"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
+echo -e "\n"
+echo -e "\n----------------------------------------"
+echo -e "\n          Run dotnet --info"
+echo -e "\n----------------------------------------"
+echo -e "\n"
 
+source ~/.bashrc
 dotnet --info
 
-echo -e "\e[0m"
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[1m              ALL DONE!"
+echo -e "\n"
+echo -e "\n----------------------------------------"
+echo -e "\n              ALL DONE!"
 echo ""
-echo -e "\e[0mGo ahead and run \e[1mdotnet new console \e[0min a new directory!"
+echo -e "\nGo ahead and run dotnet new console in a new directory!"
 echo ""
 echo ""
-echo -e "Let me know how you get on by tweeting me at \e[1;5m@pete_codes"
+echo -e "Let me know how you get on by tweeting me at @pete_codes"
 echo ""
-echo -e "\e[1m----------------------------------------"
-echo -e "\e[0m"
-
+echo -e "\n----------------------------------------"
+echo -e "\n"
 
